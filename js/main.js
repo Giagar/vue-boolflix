18b506d08654a0562a5fce0ad5570d018a8647e5
Gianpaolo <pao.barga@gmail.com>
2021-02-09 12:11:00 +0100

    commit finale: esercizio finito

---
+++ js/main.js
@@ -0,0 +1,25 @@
+new Vue({
+    el: "#root",
+
+    data: {
+        searchByTerm: "",
+        searchByTermResults: "",
+    },
+
+    methods: {
+        queryDatabase() {
+            // retrieving data from the database
+            axios
+            .get("https://api.themoviedb.org/3/search/movie?api_key=ce3b6870fdfccf78b80dcdd8f1af7e7c&query=" + this.searchByTerm)
+            .then(res => {
+                let result = res.data.results // array
+
+                this.searchByTermResults = result;
+
+                // empty the search field
+                this.searchByTerm = "";
+            })
+
+        }
+    },
+})
