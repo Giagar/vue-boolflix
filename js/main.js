90dc548eed72312f78161f83108d136ece0078f5
Gianpaolo <pao.barga@gmail.com>
2021-02-10 11:23:27 +0100

    milestone 2: bandiera fatta

--- js/main.js
+++ js/main.js
@@ -20,6 +20,22 @@ new Vue({
                 this.searchByTerm = "";
             })
 
+        },
+
+        // converting a language into a flag code
+        countryFlags(value) {
+            console.log("value", value);
+            let result = "";
+
+            switch(value) {
+                case "en":
+                    result = "us";
+                    break;
+                default:
+                    result = false;
+            }
+            console.log("result", result);
+            return result;
         }
     },
 
