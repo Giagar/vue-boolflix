4ac332a6f1774edccdb863b690576839e570e00e
Gianpaolo <pao.barga@gmail.com>
2021-02-10 13:10:32 +0100

    milestone 2 completata: richiesta per serie tv fatta

--- js/main.js
+++ js/main.js
@@ -10,9 +10,13 @@ new Vue({
         queryDatabase() {
             // retrieving data from the database
             axios
-            .get("https://api.themoviedb.org/3/search/movie?api_key=ce3b6870fdfccf78b80dcdd8f1af7e7c&query=" + this.searchByTerm)
-            .then(res => {
-                let result = res.data.results // array
+            .all([
+                axios.get("https://api.themoviedb.org/3/search/movie?api_key=ce3b6870fdfccf78b80dcdd8f1af7e7c&query=" + this.searchByTerm),
+                axios.get("https://api.themoviedb.org/3/search/tv?api_key=ce3b6870fdfccf78b80dcdd8f1af7e7c&query=" + this.searchByTerm)
+            ])
+            .then(([res1, res2]) => {
+                
+                let result = res1.data.results.concat(res2.data.results); // array
 
                 this.searchByTermResults = result;
 
