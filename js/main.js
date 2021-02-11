5fb86cd3eacc273d86e95f0c22247882041c48af
Gianpaolo <pao.barga@gmail.com>
2021-02-11 13:22:26 +0100

    impostato scorrimento orizzontale invece che verticale

--- js/main.js
+++ js/main.js
@@ -19,13 +19,11 @@ new Vue({
                 
                 let result = res1.data.results.concat(res2.data.results); // array
 
-                // check for missing poster images and substitution with a default cover img
+                // checking for missing poster images and setting a default cover img
                 result.forEach(obj => {
 // ?? va bene passare true per nn fargli fare niente - (l'assegnazione funziona, ma il percorso è sbagliato) ?? 
                     obj.poster_path ? true : obj.poster_path = "./img-fallback/default-cover.jpg"
                 });
-
-                console.log(result);
 
                 this.searchByTermResults = result;
 
