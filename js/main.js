11e3d8de791ba7a03788f4b99892aa2c0c664d59
Gianpaolo <pao.barga@gmail.com>
2021-02-12 19:47:46 +0100

    milestone 5: aggiunta chiamata attori e relativo campo

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@ new Vue({
     data: {
         searchByTerm: "",
         searchByTermResults: "",
+
+        // more info displayed
+        cast: "",
+        movieGenresList: "",
+        tvGenresList: "",
         // flag: "",
     },
 
@@ -13,11 +18,38 @@ new Vue({
             axios
             .all([
                 axios.get("https://api.themoviedb.org/3/search/movie?api_key=ce3b6870fdfccf78b80dcdd8f1af7e7c&query=" + this.searchByTerm),
-                axios.get("https://api.themoviedb.org/3/search/tv?api_key=ce3b6870fdfccf78b80dcdd8f1af7e7c&query=" + this.searchByTerm)
+                axios.get("https://api.themoviedb.org/3/search/tv?api_key=ce3b6870fdfccf78b80dcdd8f1af7e7c&query=" + this.searchByTerm),
+
+                // get movie genres list
+                axios.get("https://api.themoviedb.org/3/genre/movie/list?api_key=ce3b6870fdfccf78b80dcdd8f1af7e7c&language=en-US")
+                // get series genres list
+                // axios.get("https://api.themoviedb.org/3/genre/movie/list?api_key=ce3b6870fdfccf78b80dcdd8f1af7e7c&language=en-US")
             ])
-            .then(([res1, res2]) => {
+            .then(([resMovie, resTv, resMovieGenresList]) => {
                 
-                let result = res1.data.results.concat(res2.data.results); // array
+                let result = resMovie.data.results.concat(resTv.data.results); // array
+
+                this.movieGenresList = resMovieGenresList.data.genres;
+
+                // get genres
+                
+
+                console.log(this.movieGenresList);
+
+
+
+                // result.map(movie => {
+                //     axios
+                //     .get("https://api.themoviedb.org/3/movie/75780/credits?api_key=ce3b6870fdfccf78b80dcdd8f1af7e7c")
+                //     .then((resCast) => { // array
+
+                //         console.log(resCast.data.cast.forEach());
+                //         // let actors = cast.data.map(actor => actor.name)
+
+                //         // return {...movie, actors: actor}
+                //     })
+                // })
+                
 
                 // checking for missing poster images and setting a default cover img
                 result.forEach(obj => {
@@ -29,6 +61,8 @@ new Vue({
 
                 // empty the search field
                 this.searchByTerm = "";
+
+                // console.log(result)
             })
 
         },
@@ -48,6 +82,36 @@ new Vue({
             return `vote fa-star ${star <= Math.ceil(movie.vote_average / 2) ? 'fas' : 'far'}`;
         },
 
+        // get more data: actors and genre (max: 2 items)
+        displayMoreInfo(movie) {
+
+            // get actors
+            axios
+            .get("https://api.themoviedb.org/3/movie/" + movie.id + "/credits?api_key=ce3b6870fdfccf78b80dcdd8f1af7e7c")
+            .then(res => {
+                res = res.data.cast;
+                this.cast = res.map(actor => actor.name).slice(0, 2).join(", ")
+            })
+        },
+
+        // displayGenres(movie) {
+        //     let result =  movie.genre_ids.map(genre => {
+        //         return this.movieGenresList.filter(obj => {
+        //             return obj.id === genre
+        //         })[0].name
+        //     });
+        //     console.log(result);
+        //     return result;
+
+        // },
+
+        // cut the results (strings) to a max length
+
+    },
+
+    mounted: function() {
+        // get movie's genres list
+        
     },
     filters: {
     }
